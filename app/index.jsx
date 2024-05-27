5f50cb3e2b6e1e96928f9c9a886b7e24a326d71a
Charan <charankulal0241@gmail.com>
2024-05-27 22:46:27 +0530

    Installed nativewind and tailwindcss@3.3.2

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style="flex-1 items-center justify-center bg-white">
       <Text>Aora!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
